6d59d73189495b115153f3c4f1939965f4e7f4e3
Devin <devinshackle@gmail.com>
2013-12-11 21:49:32 +0000

    index nav bar, db name

--- views/_v_template.php
+++ views/_v_template.php
@@ -21,6 +21,46 @@
 
 <body>	
 
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<span class="navbar-brand"><?=APP_NAME?></span>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+			<ul class="nav navbar-nav">
+				<li><a href="/">Home</a></li>
+
+			<?php if($user): ?>
+				<li><a href='/users/profile'>Profile</a></li>
+				<li><a href='/posts/users'>Users</a></li>
+				<li><a href='/posts/index'>My Stream</a></li>
+				<li><a href='/posts/add'><b>Post</b></a></li>
+
+			<?php else: ?>
+
+				<li><a href='/users/signup'>Sign Up</a></li>
+
+			<?php endif; ?>
+
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			<?php if($user): ?>
+				<li><a href='/users/logout'>Logout</a></li>
+			<?php else: ?>
+				<li><a href='/users/login'>Login</a></li>
+
+			<?php endif; ?>
+			</ul>
+		</div>
+	</nav>
+
 	<?php if(isset($content)) echo $content; ?>
 
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
